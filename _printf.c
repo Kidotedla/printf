8f691b122c17b795590ee688f9699d313af23f88
kidotedla <tedlakidus0@gmail.com>
2021-11-13 09:14:25 -0800

    printf files

--- _printf.c
+++ _printf.c
@@ -1,78 +1,54 @@
-#include <unistd.h>
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- * buffer_print - print given buffer to stdout
- * @buffer: buffer to print
- * @nbytes: number of bytes to print
+ * _printf - Function that prints formatted output.
  *
- * Return: nbytes
- */
-int buffer_print(char buffer[], unsigned int nbytes)
-{
-  write(1, buffer, nbytes);
-  return (nbytes);
-}
-/**
- * buffer_add - adds a string to buffer
- * @buffer: buffer to fill
- * @str: str to add
- * @buffer_pos: pointer to buffer first empty position
+ * @format: a string composed of zero or more characters to print or use as
+ * directives that handle subsequent arguments and special characters.
  *
- * Return: if buffer filled and emptyed return number of printed char
- * else 0
- */
-int buffer_add(char buffer[], char *str, unsigned int *buffer_pos)
-{
-  int i = 0;
-  unsigned int count = 0, pos = *buffer_pos, size = BUFFER_SIZE;
-  while (str && str[i])
-    {
-      if (pos == size)
-	{
-	  count += buffer_print(buffer, pos);
-	  pos = 0;
-	}
-      buffer[pos++] = str[i++];
-    }
-  *buffer_pos = pos;
-  return (count);
-}
-/**
- * _printf - produces output according to a format
- * @format: character string
+ * Description: This function can take a variable number and type of arguments
+ * that should be printed to standard output.
  *
- * Return: the number of characters printed excluding the null byte
- * used to end output to strings
+ * Return: int
  */
 int _printf(const char *format, ...)
 {
-  va_list ap;
-  unsigned int i = 0, buffer_pos = 0, count = 0;
-  char *res_str, *aux, buffer[BUFFER_SIZE];
-  if (!format || !format[0])
-    return (-1);
-  va_start(ap, format);
-  aux = malloc(sizeof(char) * 2);
-  while (format && format[i])
-    {
-      if (format[i] == '%')
+	va_list args;
+	int i = 0, chars_printed = 0;
+
+	va_start(args, format);
+	while (format && format[i])
 	{
-	  res_str = treat_format(format, &i, ap);
-	  count += buffer_add(buffer, res_str, &buffer_pos);
-	  free(res_str);
+		if (format[i] != '%')
+		{
+			chars_printed += _putchar(format[i]);
+		}
+		else if (format[i + 1])
+		{
+			i++;
+			if (format[i] == 'c' || format[i] == 's')
+				chars_printed += format[i] == 'c' ? _putchar(va_arg(args, int)) :
+				print_string(va_arg(args, char *));
+			else if (format[i] == 'd' || format[i] == 'i')
+				chars_printed += print_num(va_arg(args, int));
+			else if (format[i] == 'b')
+				chars_printed += print_binary((unsigned int)va_arg(args, int));
+			else if (format[i] == 'r')
+				chars_printed += print_reverse(va_arg(args, char *));
+			else if (format[i] == 'R')
+				chars_printed += print_rot13(va_arg(args, char *));
+			else if (format[i] == 'o' || format[i] == 'u' ||
+			format[i] == 'x' || format[i] == 'X')
+				chars_printed += print_odh(format[i], (unsigned int)va_arg(args, int));
+			else if (format[i] == 'S')
+				chars_printed += print_S(va_arg(args, char *));
+			else if (format[i] == 'p')
+				chars_printed += print_pointer(va_arg(args, void *));
+			else
+				chars_printed += print_unknown_spec(format[i]);
+		}
+		i++;
 	}
-      else
-	{
-	  aux[0] = format[i++];
-	  aux[1] = '\0';
-	  count += buffer_add(buffer, aux, &buffer_pos);
-	}
-    }
-  count += buffer_print(buffer, buffer_pos);
-  free(aux);
-  va_end(ap);
-  if (!count)
-    count = -1;
-  return (count);
+	va_end(args);
+	return (chars_printed);
 }

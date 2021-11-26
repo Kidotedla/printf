687cf54712485b583ed10c7355566107b8237811
kidotedla <tedlakidus0@gmail.com>
2021-11-26 11:27:42 -0800

    printf

--- main.h
+++ main.h
@@ -1,22 +1,34 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdarg.h>
+#include <unistd.h>
 
-int _putchar(char c);
 int _printf(const char *format, ...);
-int print_num(long int n);
-int print_string(char *str);
-int print_binary(unsigned int num);
-int print_unknown_spec(char c);
-int print_odh(char c, unsigned int num);
-int dec_to_oct(unsigned int num);
-int dec_to_hex(char c, unsigned int num);
-int print_S(char *);
-int print_reverse(char *s);
-int print_rot13(char *c);
-int print_pointer(void *p);
+int _position(const char *s, int n);
+int _strlen(char *s);
+int _abs(int n);
+int _numlen(int n);
+char *_strcat(char *dest, char *src, int n);
+void *rev_string(char *s);
 
-#endif /* HOLBERTON_H */
+/**
+ * struct type - structure datatype
+ * @op: type arguement
+ * @f: assofiated functio
+ */
+
+typedef struct type
+{
+	char *op;
+	char *(*f)(va_list);
+} type_t;
+
+char *print_c(va_list list);
+char *print_s(va_list list);
+char *print_i(va_list list);
+char *print_bin(va_list list);
+
+#endif /* #ifndef _MAIN_H */
